3213e1b730f905d015f89b367f4501f5872a9f26
Booze33 <tebetisloh@gmail.com>
2023-07-12 16:18:42 -0500

    fixing linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many   :comments
+  has_many :comments
   has_many :likes
 
   def update_author_posts_counter
