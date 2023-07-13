6c6be22a892b366b8f7f92af853176c7f9a755f9
Booze33 <tebetisloh@gmail.com>
2023-07-13 18:24:47 -0500

    Added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
+  validates :text, presence: true
+
+  after_save :update_post_comments_counter
+
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ActiveRecord::Base
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
+  after_save :update_post_likes_counter
+
   def update_post_likes_counter
     post.update(likes_count: post.likes.count)
   end
