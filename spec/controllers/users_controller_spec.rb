b2d0f9286269b2329bbdc10a2d7b177000175e67
Booze33 <tebetisloh@gmail.com>
2023-07-18 15:49:50 -0500

    Blog app - controllers specs

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  describe "GET index" do
+    it "returns a successful response" do
+      get users_path
+      expect(response).to be_successful
+    end
+
+    it "renders the index template" do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET show" do
+    it "returns a successful response" do
+      user = User.create(name: "John")
+      get user_path(user)
+      expect(response).to be_successful
+    end
+
+    it "renders the show template" do
+      user = User.create(name: "John")
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+  end
+end
