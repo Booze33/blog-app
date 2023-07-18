b05e81c20db3d10ee29c6c6261fc880bc2ca5681
Booze33 <tebetisloh@gmail.com>
2023-07-18 15:54:14 -0500

    fixing errors

--- spec/Request/posts_controller_spec.rb
+++ spec/Request/posts_controller_spec.rb
@@ -16,24 +16,24 @@ RSpec.describe PostsController, type: :request do
   end
 
   describe 'GET #show' do
-  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVv', bio: 'Teacher', posts_counter: 0) }
-  let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0) }
+    let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVv', bio: 'Teacher', posts_counter: 0) }
+    let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0) }
 
-  before do
-    get user_post_path(user_id: user.id, id: post.id)
-  end
+    before do
+      get user_post_path(user_id: user.id, id: post.id)
+    end
 
-  after do
-    Post.destroy_all
-    User.destroy_all
-  end
+    after do
+      Post.destroy_all
+      User.destroy_all
+    end
 
-  it 'returns a successful response' do
-    expect(response).to have_http_status(:ok)
-  end
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
 
-  it "renders the 'show' template" do
-    expect(response).to render_template('show')
+    it "renders the 'show' template" do
+      expect(response).to render_template('show')
+    end
   end
-end
 end
