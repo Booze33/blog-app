ac296549fc75a14a2e275a5747270854f75126d4
Booze33 <tebetisloh@gmail.com>
2023-07-18 16:10:48 -0500

    fixing linters

--- spec/Request/posts_controller_spec.rb
+++ spec/Request/posts_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe PostsController, type: :request do
   describe 'GET #show' do
     text = 'This is my first post'
     let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVv', bio: 'Teacher', posts_counter: 0) }
-    let(:post) { Post.create(author: user, title: 'Hello', text: text , comments_counter: 0, likes_counter: 0) }
+    let(:post) { Post.create(author: user, title: 'Hello', text:, comments_counter: 0, likes_counter: 0) }
 
     before do
       get user_post_path(user_id: user.id, id: post.id)
