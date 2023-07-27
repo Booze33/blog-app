1e9cf318da0b0be68ff194adb4db42eed9db5082
Booze33 <tebetisloh@gmail.com>
2023-07-27 06:13:51 -0500

    Added email comfirmation page

---
+++ db/migrate/20230727093218_add_confirmable_columns_to_users.rb
@@ -0,0 +1,6 @@
+class AddConfirmableColumnsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+  end
+end
