d88be8542fbe1eae577dd2f681794ec84a67e1f3
Booze33 <tebetisloh@gmail.com>
2023-07-27 12:20:20 -0500

    fixing linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    if params[:id] == "sign_out"
+    if params[:id] == 'sign_out'
       sign_out_current_user
-      redirect_to new_user_session_path, notice: "You have signed out successfully!"
+      redirect_to new_user_session_path, notice: 'You have signed out successfully!'
     else
       @user = User.find(params[:id])
     end
