7122793a0099cf9bd147493bca40c6acb6431ae0
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-28 17:03:39 +0500

    Add ability class for authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,1 +1,16 @@
-# Add the code here
+class Ability
+  include CanCan::Ability
+  def initialize(user)
+    user ||= User.new
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Post
+      can :create, Comment
+      can :update, Post, author_id: user.id
+      can :destroy, Post, author_id: user.id
+      can :destroy, Comment, author_id: user.id
+    end
+  end
+end
